ffeb01ca32c2f08bb1ef987a64cd39506b00127e
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2023-09-29 22:13:36 -0300

    Optimized handling of nicknames and messages

--- post.php
+++ post.php
@@ -6,10 +6,15 @@ if(isset($_POST['sent']))
 {	 
 	 $author = addslashes($_POST['author']);
 	 $message = addslashes($_POST['message']);
-	 if(strlen($author) >= 1 && strlen($author) <= 20)
+	 if (empty($author) || strlen($author) > 20)
 	 {
-		if(strlen($message) >= 1 && strlen($message) <= 280)
+		echo "Your nickname cannot be empty or have more than 20 characters.";
+	 } else
+	 {
+		if (empty($message) || strlen($message) > 280)
 		{
+			echo "Your message cannot be empty or exceed 280 characters.";
+		} else {
 			$sql = "INSERT INTO posts (id, author, date, post)
 			VALUES (NULL,'$author',current_timestamp(),'$message')";
 			if (mysqli_query($conn, $sql)) {
@@ -21,12 +26,7 @@ if(isset($_POST['sent']))
 		" . mysqli_error($conn);
 				mysqli_close($conn);
 			}
-		} else {
-			echo "Your message cannot be empty or exceed 280 characters.";
 		}
-	 } else
-	 {
-		echo "Your nickname cannot be empty or have more than 20 characters.";
 	 }
 }
 
